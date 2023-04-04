df5eb1d3fbd931bfd1e55c336167807ac07e0dbc
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-03 18:38:46 -0700

    score page and new question

--- highscore.js
+++ highscore.js
@@ -1,64 +1,29 @@
-
-// variables pointing to html doc
-li = document.createElement("li")
-
-
-
-
-var i = 0
-var score = 0;
-
-
-
-
-
-// for scoreboard
-var ul = document.querySelector("ul")
-
-var form = document.querySelector("form");
-var labelEl = document.createElement("label");
-var inputEl = document.createElement("input");
-labelEl.textContent = "Initials";
-var submit = document.createElement("button")
-submit.textContent = "Submit";
-
-var userInfo;
-    
-// storing the users name and score in an object 
-function storeUserInfo(){
-    userInfo = {
-    // need to have .value to input the string that is the name 
-        userName:inputEl.value,
-        score: score
-    
-};
-// Need to bring in existin scores from localStorage;
-var hs = []
-hs.push(userInfo.value)
-localStorage.setItem("hs", JSON.stringify(hs));
-
-;
-// setting the userInfo into local storage
-   
-localStorage.setItem("userInfo", JSON.stringify(userInfo));
-}
-// printing the users name 
-function printScore(){
+var userScore = [];
+ul = document.querySelector("ul")
+//  printing the users name 
+function printScore() {
     ul.innerHTML = " "
-    console.log("score is:"+score)
+
     // retreiving the usersInfo from local storage 
-    var userScore = JSON.parse(localStorage.getItem('hs')) || [];
-    var li = document.createElement("li")
-    li.textContent = ("Name: "+userScore.userName + "  Score: "+userScore.score) 
-    ul.appendChild(li)
-    console.log(userScore)
-// need to clear the inner HTML
+
+    userScore = JSON.parse(localStorage.getItem('hs')) || [];
+
+    if (userScore === null) {
+        return
+    }
+
+  
+
    
+    for (var i = 0; i < userScore.length; i++) {
+        var li = document.createElement("li")
+        li.textContent = (userScore[i].userName + " - score: " + userScore[i].score)
+        ul.appendChild(li)
+        console.log(userScore[i])
+    }
+
 }
 
 
-        storeUserInfo();
-    
-       
-        printScore();
-    
+
+printScore();
--- script.js
+++ script.js
@@ -15,53 +15,59 @@ var answer4 = document.getElementById("answer4");
 
 var i = 0
 var score = 0;
+var hs;
 
 // questions 
 var qAndA = [
-    {question:"What does i stand for in typical for loops?",
-    options:["Indicate","Index","Isolate","I dunno"],
-    answer: "Index"
+    {
+        question: "What does i stand for in typical for loops?",
+        options: ["indicate", "index", "isolate", "i-dunno"],
+        answer: "index"
     },
-    {question:"Which one of these is NOT an event handler for .addEventListener?",
-    options:["click","mouseout", "submit", "over"],
-    answer: "over",
+    {
+        question: "Which one of these is NOT an event handler for .addEventListener?",
+        options: ["click", "mouseout", "submit", "over"],
+        answer: "over",
     },
-    {question:"setInterval() is counted in:",
-    options:["milliseconds", "minutes","seconds","nanoseconds"],
-    answer: "milliseconds"
+    {
+        question: "setInterval() is counted in:",
+        options: ["milliseconds", "minutes", "seconds", "nanoseconds"],
+        answer: "milliseconds"
+    },
+    {
+        question: "Where can you find the console?",
+        options: ["inside my dev tools", "printed on the page", "it doesn't do anything", "in my .html file "],
+        answer: "inside my dev tools"
     }
- ]   
+]
 
- console.log(qAndA[0].answer)
- console.log(qAndA[1].answer)
- console.log(qAndA[2].answer)
+console.log(qAndA[0].answer)
+console.log(qAndA[1].answer)
+console.log(qAndA[2].answer)
 
 
 
 var qAndALength = Object.keys(qAndA).length
 
-function whichQandA(){
-    console.log("qandALength: "+qAndALength)
-    if(i < qAndALength){
-    question.textContent = qAndA[i].question;
-    answer1.textContent = qAndA[i].options[0];
-    answer2.textContent = qAndA[i].options[1];
-    answer3.textContent = qAndA[i].options[2];
-    answer4.textContent = qAndA[i].options[3];
-      //  i++
+function whichQandA() {
+    console.log("qandALength: " + qAndALength)
+    if (i < qAndALength) {
+        question.textContent = qAndA[i].question;
+        answer1.textContent = qAndA[i].options[0];
+        answer2.textContent = qAndA[i].options[1];
+        answer3.textContent = qAndA[i].options[2];
+        answer4.textContent = qAndA[i].options[3];
+        
     }
-    if(i === qAndALength){
-      //  i++
+    if (i === qAndALength) {
+        
         console.log("i is equal to length")
     }
-    if(i === qAndALength+1){
+    if (i === qAndALength + 1) {
         displayForm()
         console.log("i is greather than length")
     }
 }
-
-
-
 
 
 // creating form elements 
@@ -80,108 +86,103 @@ var timeGiven = 20;
 
 // clicking the start button will start the timer, and show the first questions 
 // if the timer runs out the user form will be shown 
-startButton.addEventListener("click",function(event){
+startButton.addEventListener("click", function (event) {
     startButton.setAttribute("class", "hide");
     console.log('hey');
-    var timeInterval = setInterval(function(){
+    var timeInterval = setInterval(function () {
         timeGiven--
-        if(timeGiven >=1){
+        if (timeGiven >= 1) {
             count.textContent = (timeGiven + " seconds left")
-            
+
         }
-          // if the timer is equal to zero then the form for the scoreboard will show 
-        if(timeGiven <= 0 ){
+        // if the timer is equal to zero then the form for the scoreboard will show 
+        if (timeGiven <= 0) {
             count.textContent = ("Game Over!");
             clearInterval(timeInterval);
-           displayForm()
-            
+            displayForm()
+
         }
-        if(i === qAndALength && timeGiven > 0){
-            count.textContent = (" You finished with "+timeGiven+ " seconds left!");
+        if (i === qAndALength && timeGiven > 0) {
+            count.textContent = (" You finished with " + timeGiven + " seconds left!");
             clearInterval(timeInterval)
             displayForm()
         }
 
-    },1000)
+    }, 1000)
     // first questions presented
 
-     whichQandA()
+    whichQandA()
+
     replayButton.textContent = "Replay"
     replayButton.setAttribute("class", "button");
-     })
+})
 
 
-    //  if anywhere in the button container is clicked then:
-    answerContainer.addEventListener("click",function(event){
-        event.stopPropagation()
-        console.log(i +"for each click")
-        console.log("event.target.textContent = ", event.target.textContent);
-        function scoring(){
-            
-            if(i < qAndA.length){
-                console.log(qAndA[i].answer)
-                
-                if(event.target.textContent === qAndA[i].answer){
+//  if anywhere in the button container is clicked then:
+answerContainer.addEventListener("click", function (event) {
+    event.stopPropagation()
+    console.log(i + "for each click")
+    console.log("event.target.textContent = ", event.target.textContent);
+    function scoring() {
+
+        if (i < qAndA.length) {
+            console.log(qAndA[i].answer)
+
+            if (event.target.textContent === qAndA[i].answer) {
                 score++
                 console.log("correct")
-                }
-                else{                
-                    timeGiven = timeGiven - 5
-                    console.log("wrong");
-                    if(timeGiven < 5){
-                        timeGiven = 0
-                    }
-                    count.textContent = (timeGiven + " seconds left");
-                }
             }
-            i++;
+            else {
+                timeGiven = timeGiven - 5
+                console.log("wrong");
+                if (timeGiven < 5) {
+                    timeGiven = 0
+                }
+                count.textContent = (timeGiven + " seconds left");
+            }
         }
-        scoring()
-       
-        whichQandA()
-           
-    
+        i++;
     }
+    scoring()
 
-     
-    )
+    whichQandA()
 
 
+}
 
-    
-    
-   
-        
+
+)
+
 // storing the users name and score in an object 
-function storeUserInfo(){
+function storeUserInfo() {
     var userInfo = {
-    // need to have .value to input the string that is the name 
-        userName:inputEl.value,
+        // need to have .value to input the string that is the name 
+        userName: inputEl.value,
         score: score
-    
-};
-// Need to bring in existin scores from localStorage;
-var hs = JSON.parse(localStorage.getItem('hs')) || [];
-hs.push(userInfo);
-// setting the userInfo into local storage
+
+    };
+    // Need to bring in existin scores from localStorage;
+    hs = JSON.parse(localStorage.getItem('hs')) || [];
+    hs.push(userInfo);
+    // setting the userInfo into local storage
     localStorage.setItem("hs", JSON.stringify(hs));
     localStorage.setItem("userInfo", JSON.stringify(userInfo));
 }
 // printing the users name 
-function printScore(){
+function printScore() {
     ol.innerHTML = " "
-    console.log("score is:"+score)
+    console.log("score is:" + score)
     // retreiving the usersInfo from local storage 
     var lastUser = JSON.parse(localStorage.getItem("userInfo"))
     var li = document.createElement("li")
-    li.textContent = ("Name: "+lastUser.userName + "  Score: "+lastUser.score) 
+    li.textContent = ("Name: " + lastUser.userName + "  Score: " + lastUser.score)
     ol.appendChild(li)
-// need to clear the inner HTML
-   
+    // need to clear the inner HTML
+
 }
 
 // triggered by the end of the timer 
-function displayForm(){
+function displayForm() {
     var qAndA = document.getElementById("qAndA-div");
     qAndA.setAttribute("class", "hide");
     // event.preventDefault();
@@ -189,26 +190,26 @@ function displayForm(){
     form.appendChild(labelEl);
     form.appendChild(inputEl);
     form.appendChild(submit)
-    
-    submit.addEventListener("click",function(event){
+
+    submit.addEventListener("click", function (event) {
         event.preventDefault();
         storeUserInfo();
-    // ol.innerHTML = ""
-       
+        // ol.innerHTML = ""
+
         printScore();
-        
+
     }
 
     )
-   
-    }
-    
-    replayButton.addEventListener("click",function(event){
-        window.location.reload()
-    })
 
-  
+}
+
+replayButton.addEventListener("click", function (event) {
+    window.location.reload()
+})
+
+
         // labelEl.remove;
         // inputEl.remove;
         // submit.remove;
-    
+
